b86fc48f4177e220e189731e1faa071a092a4d51
NileshRavindraP <nileshrp239@gmail.com>
2025-12-03 23:17:38 +0530

    added some skills into techStack

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -15,11 +15,24 @@ export const socialLinks = {
 };
 
 export const techItems = [
-  { id: 1, logo: '/html.png', name: 'HTML', brand: '#E34F26' },
-  { id: 2, logo: '/css.jpg', name: 'CSS', brand: '#1572B6' },
-  { id: 3, logo: '/CPP.png', name: 'C++', brand: '#00599C' },
-  { id: 4, logo: '/javascript.png', name: 'JavaScript', brand: '#F7DF1E' },
-  { id: 5, logo: '/SQL.png', name: 'SQL', brand: '#3178C6' },
-  { id: 6, logo: '/reactjs.png', name: 'ReactJs', brand: '#61DAFB' },
-  { id: 7, logo: '/node.png', name: 'NodeJs', brand: '#43853D' },
+  // Frontend Core
+  { id: 1, logo: '/javascript.png', name: 'JavaScript', brand: '#F7DF1E' },
+  { id: 2, logo: '/reactjs.png', name: 'ReactJs', brand: '#61DAFB' },
+  { id: 3, logo: '/html.png', name: 'HTML', brand: '#E34F26' },
+  { id: 4, logo: '/css.jpg', name: 'CSS', brand: '#1572B6' },
+  
+  // Backend & Runtime
+  { id: 5, logo: '/node.png', name: 'NodeJs', brand: '#43853D' },
+  { id: 6, logo: '/express.png', name: 'ExpressJs', brand: '#43853D' },
+  { id: 7, logo: '/java.png', name: 'Java', brand: '#43853D' },
+  { id: 8, logo: '/springBoot.jpg', name: 'Spring boot', brand: '#43853D' },
+  
+  // Database & API
+  { id: 9, logo: '/SQL.png', name: 'SQL', brand: '#3178C6' },
+  { id: 10, logo: '/mongodb.png', name: 'MongoDB', brand: '#13AA52' },
+  { id: 11, logo: '/Apigee.jpeg', name: 'APIGEE', brand: '#43853D' },
+  
+  // Additional
+  { id: 12, logo: '/CPP.png', name: 'C++', brand: '#00599C' },
+  { id: 13, logo: '/DSA.png', name: 'DSA', brand: '#FF6B6B' },
 ];
